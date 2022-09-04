2eef17fd2734d00dd8e79eaf6a72530db7ac255b
chenxin <1945827520@qq.com>
2022-09-04 17:18:33 +0800

    Java基础环境测试

---
+++ src/_01HelloWord/HelloWord.java
@@ -0,0 +1,16 @@
+package _01HelloWord;
+
+/**
+ * @author: ChenXin
+ * @createTime: 2022-09-02 23:02
+ * @Description: Java基础环境测试
+ * @version: 1.0.0
+ */
+
+public class HelloWord {
+    public static void main(String[] args) {
+        //运行成功则表示Java基础环境已经搭建完成
+        System.out.println("HelloWord");
+        System.out.println("这是第一个Java程序");
+    }
+}
